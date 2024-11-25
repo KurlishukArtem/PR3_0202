9694c2da0a494062dd84d51bce36db96601e9d01
Hat <cool97438@gmail.com>
2024-11-25 16:09:29 +0500

    Созданный метод Send. Kurlishuk

--- Snake_Kurlishuk/Program.cs
+++ Snake_Kurlishuk/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using Common;
+using System.Net.Http.Headers;
 
 namespace Snake_Kurlishuk
 {
@@ -16,5 +20,39 @@ namespace Snake_Kurlishuk
         static void Main(string[] args)
         {
         }
+
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeIObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+
+                    //отпавляем данные
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    //выводим данные
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+                }
+                finally
+                {
+                    //если все выполнилось успешно, уматываем из этого метода
+                    sender.Close();
+                }
+
+
+            }
+        }
+
     }
 }
