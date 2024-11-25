cfbbad73334a02619d6ce99ea36de31ee5fd6508
Hat <cool97438@gmail.com>
2024-11-25 14:20:01 +0500

    Добавьте файлы проекта.

---
+++ Common/Snakes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            //система координат очевидно
+            public Point(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+            public Point() { }
+        }
+        //направление движения змеи
+        public enum Direction 
+        {
+            Left,
+            Right, 
+            Up,
+            Down,
+            Start  
+        }
+
+        //точки из которых состоит Змея
+        public List<Point> Points = new List<Point>();
+        //направление движения в котором двигается змея
+        public List<Direction> direction = Direction.Start;
+        public bool GameOver = false;   
+    }
+}
---
+++ Snake_Kurlishuk/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Snake_Kurlishuk")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Snake_Kurlishuk")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9c8aabde-723f-481d-9157-ec97b29566bf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
