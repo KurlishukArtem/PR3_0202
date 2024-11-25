7692f70e9bcd2445d60947c0db5441c67ecfc5e4
Hat <cool97438@gmail.com>
2024-11-25 14:26:32 +0500

    Модель представления данных пользователя. Kurlishuk

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    //отвечающий за данные пользователя, а именно IP-адрес, прослушиваемый порт, имя игрока, ID змеи:
+    public class ViewModelUserSettings
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public int IdSnake = -1;
+    }
+}
