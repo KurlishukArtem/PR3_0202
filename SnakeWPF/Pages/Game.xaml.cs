674f05bcb926259d724092df75fca300651b7d22
Hat <cool97438@gmail.com>
2024-12-02 02:18:47 +0500

    Программирование страницы Game. Kurlishuk

--- SnakeWPF/Pages/Game.xaml.cs
+++ SnakeWPF/Pages/Game.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Common;
+
 
 namespace SnakeWPF.Pages
 {
@@ -20,9 +22,107 @@ namespace SnakeWPF.Pages
     /// </summary>
     public partial class Game : Page
     {
+        public int StepCadr = 0;
         public Game()
         {
             InitializeComponent();
+        }
+        public void CreateUI()
+        {
+            //выполняем вне потока
+            Dispatcher.Invoke(() =>
+            {
+                //если кадр 0 то кадр 1
+                if (StepCadr == 0) StepCadr = 1;
+                else StepCadr = 0;
+                //чистим интерфейс
+                canvas.Children.Clear();
+                //перебираем точки змеи
+                for (int iPoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayers.Points.Count - 1; iPoint >= 0; iPoint--)
+                {
+                    Snakes.Point SnakePoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayers.Points[iPoint];
+
+                    // Смещение точек змеи
+                    if (iPoint != 0)
+                    {
+                        //получаем следующую точку змеи
+                        Snakes.Point NextSnakePoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayers.Points[iPoint - 1];
+                        // Если точка находится по горизонтали
+                        if (SnakePoint.X > NextSnakePoint.X || SnakePoint.X < NextSnakePoint.X)
+                        {
+                            if (iPoint % 2 == 0)
+                            {
+                                //если кадр четный
+                                if (iPoint % 2 == 0)
+                                    SnakePoint.Y -= 1;
+                                else
+                                    SnakePoint.Y += 1;
+
+
+                            }
+                            else {
+                                //если кадр четный
+                                if (iPoint % 2 == 0)
+                                    SnakePoint.Y += 1;
+                                else
+                                    SnakePoint.Y -= 1;
+                            }
+                        }
+                        if (SnakePoint.Y > NextSnakePoint.Y || SnakePoint.Y < NextSnakePoint.Y)
+                        {
+                            if (iPoint % 2 == 0)
+                            {
+                                //если кадр четный
+                                if (iPoint % 2 == 0)
+                                    SnakePoint.X -= 1;
+                                else
+                                    SnakePoint.X += 1;
+
+
+                            }
+                            else
+                            {
+                                //если кадр четный
+                                if (iPoint % 2 == 0)
+                                    SnakePoint.X += 1;
+                                else
+                                    SnakePoint.X -= 1;
+                            }
+                        }
+                    }
+                    Brush Color;
+                    if (iPoint == 0)
+                        Color = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 0, 127, 14));
+                    else
+                        Color = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 0, 198, 19));
+                    //рисуем точку
+                    Ellipse ellipse = new Ellipse();
+                    {
+                        Width = 20;
+                        Height = 20;
+                        Margin = new Thickness(SnakePoint.X - 10, SnakePoint.Y - 10, 0, 0),
+                        //Цвет точки
+                        Fill = Color;
+                        // обводка
+                        Stroke = Brushes.Black;
+                    };
+                    canvas.Children.Add(ellipse);
+                }
+                //отрисовка яблока
+                //Изображение яблока
+                ImageBrush myBrush = new ImageBrush();
+                myBrush.ImageSource = new BitmapImage(new Uri($"pack://application:,,,/Image/Apple.png"));
+                //яблоко на UI
+                Ellipse point = new Ellipse();
+                {
+                    Width = 40;
+                    Height = 40;
+                    Margin = new Thickness(MainWindow.mainWindow.ViewModelGames.Points.X - 20,
+                        MainWindow.mainWindow.ViewModelGames.Points.Y - 20,0,0),
+                    Fill = myBrush
+                };
+                canvas.Children.Add(point);
+            });
         }
     }
 }
