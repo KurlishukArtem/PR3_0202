8b809b8f2b9b4dd1b6d4677eb36291a8aa1adb31
Hat <cool97438@gmail.com>
2024-12-01 23:47:15 +0500

    Метод прослушивания ответов от сервера. Kurlishuk

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Common;
+using System.Windows.Media.Animation;
+using Newtonsoft.Json;
 
 namespace SnakeWPF
 {
@@ -46,6 +48,85 @@ namespace SnakeWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void StartReciver()
+        {
+            //создаем поток для прослушивания канала
+            tRec = new Thread(new ThreadStart(Receiver));
+            tRec.Start();
+        }
+
+        public void OpenPages(Page PageOpen)
+        {
+            DoubleAnimation startAnimation = new DoubleAnimation();
+            //задаем начальное положение анимации
+            startAnimation.From = 1;
+            startAnimation.To = 0;
+            //задаем время анимации
+            startAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            //подписываемся на выполенение анимации
+            startAnimation.Completed += delegate
+            {
+                //переключаем страницу
+                frame.Navigate(PageOpen);
+                //создаем конечную анимацию
+                DoubleAnimation endAnimation = new DoubleAnimation();
+                //задаем начальное и конечное положение
+                endAnimation.From = 0;
+                endAnimation.To = 1;
+                endAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                //воспроизводим анимацию на frame, анимацияч прозрачности
+                frame.BeginAnimation(OpacityProperty, endAnimation);
+            };
+            //воспроизводим анимацию на frame, анимацияч прозрачности
+            frame.BeginAnimation(OpacityProperty, startAnimation);
+        }
+        public void Receiver()
+        {
+            //создаем клиент для обслуживания
+            receivingUdpClient = new UdpClient(int.Parse(ViewModelUserSettings.Port));
+            IPEndPoint RemoteIpPoint = null;
+
+            try
+            {
+                //слушаем постоянно
+                while (true)
+                {
+                    //Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                    //преобразуем и отображаем данные
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    //если у нас не существует данных от сервера
+                    if (ViewModelGames == null)
+                    {
+                        //говорим что выполняем вне потока
+                        Dispatcher.Invoke(() =>
+                        {
+                            OpenPages(Game);
+                        });
+                    }
+                    ViewModelGames = JsonConvert.DeserializeObject<ViewModelGames>(returnData.ToString());
+                    //если игрок проиграл
+                    if (ViewModelGames.SnakesPlayers.GameOver)
+                    {
+                        //выполняем вне потока
+                        Dispatcher.Invoke(() =>
+                        {
+                            //открываем окно с окончанием игры
+                            OpenPages(new Pages.EndGame());
+                        });
+                    }
+                    else
+                    {
+                        //Вызываем создание UI
+                        Game.CreateUI();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+            }
         }
     }
 }
